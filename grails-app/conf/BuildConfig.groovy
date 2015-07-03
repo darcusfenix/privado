2c9fafb7c6988f4bdd0aec6005bacef5c7c20b66
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2015-07-03 11:49:00 -0500

    #1, Updating Config
    Changing url mappings from springsecurity core and updating plugins
    Adding resources pluging, asset plugin removed

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -60,13 +60,14 @@ grails.project.dependency.resolution = {
         // plugins for the compile step
         compile ":scaffolding:2.1.2"
         compile ':cache:1.1.8'
-        compile ":asset-pipeline:2.1.5"
+        //compile ":asset-pipeline:2.1.5"
         compile ":spring-security-core:2.0-RC5"
         // plugins needed at runtime but not for compilation
         runtime ":hibernate4:4.3.8.1" // or ":hibernate:3.6.10.18"
         runtime ":database-migration:1.4.0"
-        //runtime ":jquery:1.11.1"
-
+        runtime ":jquery:1.11.1"
+        runtime ":resources:1.2.14"
+        //compile ":angularjs-resources:1.4.1"
         // Uncomment these to enable additional asset-pipeline capabilities
         //compile ":sass-asset-pipeline:1.9.0"
         //compile ":less-asset-pipeline:1.10.0"
