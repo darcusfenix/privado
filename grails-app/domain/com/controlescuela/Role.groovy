f917f9eb9aaf5846fcf6a0e4dac2eadc486be386
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2015-07-04 15:22:21 -0500

    #1, Changing Spring Security Domains
    Removing all SSecurity stuff

--- grails-app/domain/com/controlescuela/Role.groovy
+++ grails-app/domain/com/controlescuela/Role.groovy
@@ -2,30 +2,7 @@ package com.controlescuela
 
 class Role implements Serializable {
 
-    private static final long serialVersionUID = 1
-
     String authority
-
-    Role(String authority) {
-        //this()
-        this.authority = authority
-    }
-
-    @Override
-    int hashCode() {
-        authority?.hashCode() ?: 0
-    }
-
-    @Override
-    boolean equals(other) {
-        is(other) || (other instanceof Role && other.authority == authority)
-    }
-
-    @Override
-    String toString() {
-        authority
-    }
-
     static constraints = {
         authority blank: false, unique: true
     }
--- grails-app/domain/com/controlescuela/User.groovy
+++ grails-app/domain/com/controlescuela/User.groovy
@@ -1,65 +1,24 @@
 package com.controlescuela
 
-class User implements Serializable {
-
-    private static final long serialVersionUID = 1
-
-    transient springSecurityService
-
+class User {
+    String name
+    String lastName
+    String phoneNumber
+    String address
     String username
     String password
     boolean enabled = true
-    boolean accountExpired
-    boolean accountLocked
-    boolean passwordExpired
-
-    User(String username, String password) {
-        //this()
-        this.username = username
-        this.password = password
-    }
-
-    @Override
-    int hashCode() {
-        username?.hashCode() ?: 0
-    }
-
-    @Override
-    boolean equals(other) {
-        is(other) || (other instanceof User && other.username == username)
-    }
-
-    @Override
-    String toString() {
-        username
-    }
-
-    Set<Role> getAuthorities() {
-        UserRole.findAllByUser(this)*.role
-    }
-
-    def beforeInsert() {
-        encodePassword()
-    }
-
-    def beforeUpdate() {
-        if (isDirty('password')) {
-            encodePassword()
-        }
-    }
-
-    protected void encodePassword() {
-        password = springSecurityService?.passwordEncoder ? springSecurityService.encodePassword(password) : password
-    }
-
-    static transients = ['springSecurityService']
 
     static constraints = {
-        username blank: false, unique: true
-        password blank: false
+        username nullable: true, blank: true, unique: true
+        password nullable: true, blank: true
+        address nullable: true
+        name nullable:true, blank:true
+        lastName nullable: true, blank: true
+        phoneNumber nullable: false
     }
 
     static mapping = {
-        password column: '`password`'
+        password column: 'password'
     }
 }
