0dccc2a24840e830e232205670e9a1caebfc9e31
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2015-07-05 00:02:41 -0500

    #1, Updating user controller
    Fixing some problems with delete method and crud basics

--- grails-app/controllers/com/controlescuela/UserController.groovy
+++ grails-app/controllers/com/controlescuela/UserController.groovy
@@ -7,10 +7,7 @@ class UserController {
     static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
 
     def index() {
-        log.error("OK")
-        log.error("OK")
-        log.error("OK")
-        render User.list([max:params.max]) as JSON
+        render User.list([max: params.int('max')]) as JSON
     }
 
     def create() {
@@ -18,7 +15,7 @@ class UserController {
     }
 
     def save(User userInstance) {
-        if(userInstance.save(flush:true, failOnError: true)){
+        if (userInstance.save(flush: true, failOnError: true)) {
             response.status = 200
             render userInstance as JSON
         } else {
@@ -27,29 +24,27 @@ class UserController {
         }
     }
 
-    def edit(Integer id){
+    def edit(Integer id) {
         render User.findById(id ?: params.int("id")) as JSON
     }
 
-    def show(Integer id){
+    def show(Integer id) {
         render User.findById(id ?: params.int("id")) as JSON
     }
 
-    def update(User userInstance){
-        if(userInstance.save(flush: true)){
+    def update(User userInstance) {
+        if (userInstance.save(flush: true)) {
             render userInstance as JSON
         } else {
-            response.status = 401
+            response.status = 500
             render userInstance.errors as JSON
         }
     }
 
-    def delete(Integer id){
-        def user =  User.findById(id ?: params.int("id"))
-        if(user.delete(flush:true)){
-            response.status = 200
-            def respuesta = [message:"Eliminado yay!"]
-            render respuesta as JSON
-        }
+    def delete(Integer id) {
+        def user = User.findById(id ?: params.int("id"))
+        user.delete(flush: true)
+        response.status = 200
+        render([message: "Eliminado"] as JSON)
     }
 }
