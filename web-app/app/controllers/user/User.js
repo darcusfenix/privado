c2c0b85df274ec4295dcf9ed07dee3124e461eae
Juan <juancvfenix@gmail.com>
2015-07-05 20:36:12 -0500

    #4, Cambio hecho para iniciar el plugin datatable a mi lista de usuarios
    Es necesario después del callback hacer esto para que se puede iniciar el plugin, esto aplicará para todos los plugins que se usarán
    El setTimeout se debe resisar, no parece ser la mejor forma

--- web-app/app/controllers/user/User.js
+++ web-app/app/controllers/user/User.js
@@ -2,7 +2,12 @@
  * Created by alberto on 3/07/15.
  */
 function UserListController($scope, User) {
+    console.log(User.query());
     $scope.userList = User.query();
+
+    setTimeout(function(){
+        TableAdvanced.init();
+    }, 1000);
 };
 
 function UserEditController($scope, $location, $routeParams, User) {
