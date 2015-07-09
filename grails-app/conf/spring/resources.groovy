8ec042a416e2d6ff3ddb2080e24a69ad797232e8
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2015-07-09 02:42:10 -0500

    #1, Updating spring resources
    registering marshaller domain class

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,12 @@
+import com.controlescuela.marshallers.CustomObjectMarshallers
+import com.controlescuela.marshallers.UserMarshaller
+
 // Place your Spring DSL code here
 beans = {
+    customObjectMarshallers(CustomObjectMarshallers) {
+        marshallers = [
+                new UserMarshaller()
+        ]
+    }
+
 }
