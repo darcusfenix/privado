1de9fd5c3bb94869e5d01019f3065be70603c3af
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2015-07-09 00:48:00 -0500

    #1, Creating roles on app startup
    Adding findOrSave methods for creating basic roles

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,13 @@
+import com.controlescuela.*
 class BootStrap {
 
     def init = { servletContext ->
+        Role alumno = Role.findOrSaveWhere([authority: 'ROLE_ALUMNO'])
+        Role empleado = Role.findOrSaveWhere([authority: 'ROLE_ALUMNO'])
+        Role admin = Role.findOrSaveWhere([authority: 'ROLE_ADMIN'])
+        Role superAdmin = Role.findOrSaveWhere([authority: 'ROLE_SU'])
     }
     def destroy = {
+
     }
 }
