4c51cb54e4e9aa46437843086463b281f8f91764
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2015-07-09 00:46:22 -0500

    #1, Updating User domain, new fields

--- grails-app/domain/com/controlescuela/User.groovy
+++ grails-app/domain/com/controlescuela/User.groovy
@@ -8,17 +8,40 @@ class User {
     String username
     String password
     boolean enabled = true
+    String mobileNumber
+    String email
+    String socialNetworkUrl
 
-    static constraints = {
-        username nullable: true, blank: true, unique: true
-        password nullable: true, blank: true
-        address nullable: true
-        name nullable:true, blank:true
-        lastName nullable: true, blank: true
-        phoneNumber nullable: false
+    // Spring security methods
+
+    @Override
+    int hashCode() {
+        username?.hashCode() ?: 0
     }
 
-    static mapping = {
-        password column: 'password'
+    @Override
+    boolean equals(other) {
+        is(other) || (other instanceof User && other.username == username)
+    }
+
+    @Override
+    String toString() {
+        username
+    }
+
+    Set<Role> getAuthorities() {
+        UserRole.findAllByUser(this)*.role
+    }
+
+    static constraints = {
+        username nullable: false, blank: false, unique: true
+        password nullable: false, blank: false
+        address nullable: false, blank: false
+        name nullable: false, blank: false, maxSize: 100
+        lastName nullable: true, blank: true, maxSize: 100
+        phoneNumber nullable: false, maxSize: 20
+        mobileNumber nullable: true, maxSize: 20
+        email nullable: false, blank: false, email: true
+        socialNetworkUrl nullable: true, blank: true, url: true
     }
 }
