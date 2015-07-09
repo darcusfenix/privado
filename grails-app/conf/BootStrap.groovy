9419d2377671e8b80fa8fcd1b903e225b4450d12
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2015-07-09 02:42:31 -0500

    #1, Registering marshaller on startup

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,11 +1,28 @@
 import com.controlescuela.*
+import org.springframework.web.context.support.WebApplicationContextUtils
+
 class BootStrap {
 
     def init = { servletContext ->
-        Role.findOrSaveWhere([authority: 'ROLE_ALUMNO'])
+        def springContext = WebApplicationContextUtils.getWebApplicationContext(servletContext)
+        springContext.getBean("customObjectMarshallers").register()
+        //
+        Role alumno = Role.findOrSaveWhere([authority: 'ROLE_ALUMNO'])
         Role.findOrSaveWhere([authority: 'ROLE_EMPLEADO'])
         Role.findOrSaveWhere([authority: 'ROLE_ADMIN'])
         Role.findOrSaveWhere([authority: 'ROLE_SU'])
+        User pepo = new User()
+        pepo.address = "test"
+        pepo.email = "jresendiz27@gmail.com"
+        pepo.enabled = true
+        pepo.name = "Alberto"
+        pepo.lastName = "Reséndiz"
+        pepo.mobileNumber = "12345679"
+        pepo.phoneNumber = "12345679"
+        pepo.password = "test"
+        pepo.username = "pepo27"
+        pepo.save()
+        UserRole.create(pepo, alumno, true)
     }
     def destroy = {
 
