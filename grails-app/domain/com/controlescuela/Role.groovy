61d3eb9205584284db772a40ce968695d57da2ac
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2015-07-09 00:46:47 -0500

    #1, Updating role
    Considering integration with Spring Security Plugin

--- grails-app/domain/com/controlescuela/Role.groovy
+++ grails-app/domain/com/controlescuela/Role.groovy
@@ -3,6 +3,26 @@ package com.controlescuela
 class Role implements Serializable {
 
     String authority
+
+    Role(String authority){
+        this.authority = authority
+    }
+
+    @Override
+    int hashCode() {
+        authority?.hashCode() ?: 0
+    }
+
+    @Override
+    boolean equals(other) {
+        is(other) || (other instanceof Role && other.authority == authority)
+    }
+
+    @Override
+    String toString() {
+        authority
+    }
+
     static constraints = {
         authority blank: false, unique: true
     }
