55766ae073a4f056d072402f4660ed3ae22f6c0b
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2015-07-09 01:12:33 -0500

    #1, Fixing typo on Bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -2,10 +2,10 @@ import com.controlescuela.*
 class BootStrap {
 
     def init = { servletContext ->
-        Role alumno = Role.findOrSaveWhere([authority: 'ROLE_ALUMNO'])
-        Role empleado = Role.findOrSaveWhere([authority: 'ROLE_ALUMNO'])
-        Role admin = Role.findOrSaveWhere([authority: 'ROLE_ADMIN'])
-        Role superAdmin = Role.findOrSaveWhere([authority: 'ROLE_SU'])
+        Role.findOrSaveWhere([authority: 'ROLE_ALUMNO'])
+        Role.findOrSaveWhere([authority: 'ROLE_EMPLEADO'])
+        Role.findOrSaveWhere([authority: 'ROLE_ADMIN'])
+        Role.findOrSaveWhere([authority: 'ROLE_SU'])
     }
     def destroy = {
 
