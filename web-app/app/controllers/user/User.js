8e8516e8812ac13fc4b4070fab724881d83e7e3f
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2015-07-10 01:18:16 -0500

    #1, Angular User controller updated
    Crud working as expected

--- web-app/app/controllers/user/User.js
+++ web-app/app/controllers/user/User.js
@@ -10,15 +10,18 @@ function UserListController($scope, $location, $rootScope, User, Role) {
 function UserEditController($scope, $location, $routeParams, $rootScope, User, Role) {
     $rootScope.location = $location.path();
     $scope.userInstance = User.get({id: $routeParams.id});
-    $scope.roles = Role.query();
-    $scope.updateUser = function updateUser() {
-        $scope.userInstance = $scope.userInstance.$update({roleId: $scope.userInstance.authority.id});
-        $location.path("/user/show/" + $routeParams.id);
+    $scope.roles = Role.query(function roles(data) {
+        $scope.roles = data;
+    });
+    $scope.updateUser = function updateUser(valid) {
+        if (valid) {
+            $scope.userInstance = $scope.userInstance.$update({roleId: $scope.userInstance.authority.id});
+            $location.path("/user/show/" + $routeParams.id);
+        }
     };
     $scope.cancelar = function cancelar() {
         $location.path("/user/show/" + $routeParams.id);
     };
-
 };
 
 function UserShowController($scope, $location, $routeParams, $rootScope, User) {
@@ -40,11 +43,27 @@ function UserCreateController($scope, $location, $rootScope, User, Role) {
     $rootScope.location = $location.path();
     $scope.errors = [];
     $scope.userInstance = User.create();
-    $scope.roles = Role.query();
-    $scope.saveUser = function saveUser() {
-        $scope.userInstance = $scope.userInstance.$save({roleId: $scope.userInstance.authority.id}, function savedUser(data) {
-            $location.path("/user/show/" + data.id);
-        });
+    $scope.roles = Role.query(function roles(data) {
+        $scope.roles = data;
+    });
+    $scope.saveUser = function saveUser(valid, $event) {
+        $event.preventDefault();
+        if (valid) {
+            $scope.userInstance.$save(
+                {
+                    roleId: $scope.userInstance.authority.id
+                }, function (data) {
+                    $scope.userInstance = data.user;
+                    $location.path("/user/show/" + data.user.id);
+                    $rootScope.message = data.message;
+                }, function (error) {
+                    console.log(error);
+                    $scope.errors = error.data.errors;
+                });
+            return false;
+        } else {
+            return false;
+        }
 
     };
     $scope.cancelar = function cancelar() {
