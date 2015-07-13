c703271d77d2b147167a4305a4d73c792dbed044
Juan <juancvfenix@gmail.com>
2015-07-13 02:02:09 -0500

    #4, working with controller's vouchersPayment
    * methods needed to service's controller that I used
    * methods needed to typeService's controller that I used
    * all my methods for voucherPayment

--- grails-app/controllers/com/controlescuela/TypeServiceController.groovy
+++ grails-app/controllers/com/controlescuela/TypeServiceController.groovy
@@ -1,10 +1,14 @@
 package com.controlescuela
 
+import com.ed.service.TypeService
 import grails.converters.JSON
 
 class TypeServiceController {
 
     def index() {
-        render TypeService.list([max: params.int('max')]) as JSON
+        render TypeService.list() as JSON
+    }
+    def show(Integer id){
+        render  (TypeService.findById(id ?: params.int("id")) as JSON)
     }
 }
