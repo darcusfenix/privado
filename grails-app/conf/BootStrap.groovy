bea6f65bde79078c547fdbf5e207a27f2cb97858
Juan <juancvfenix@gmail.com>
2015-07-13 02:17:48 -0500

    #4,  I added objects to the database I used

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -46,15 +46,30 @@ class BootStrap {
 
         StateVoucher stateVoucher = new StateVoucher()
         stateVoucher.name = "pendiente"
-        stateVoucher.description = "esta es una descripción"
+        stateVoucher.description = "Este comprobante debe ser revisado"
         stateVoucher.save();
+
+        StateVoucher anotherstateVoucher = new StateVoucher()
+        anotherstateVoucher.name = "aprobado"
+        anotherstateVoucher.description = "este comprobante ha sido revisado y aprobado"
+        anotherstateVoucher.save();
 
 
 
         TypeService typeService = new TypeService()
         typeService.description = "esta es otra descripción"
-        typeService.name = "curso muy rápido"
+        typeService.name = "Curso Intensio"
         typeService.save();
+
+        TypeService anothertypeService = new TypeService()
+        anothertypeService.description = "esta es otra descripción"
+        anothertypeService.name = "Curso online"
+        anothertypeService.save();
+
+        TypeService othertypeService = new TypeService()
+        othertypeService.description = "esta es otra descripción"
+        othertypeService.name = "Examen simulacro"
+        othertypeService.save();
 
 
         Service service = new Service()
@@ -68,26 +83,63 @@ class BootStrap {
 
         Service anotherService = new Service()
         anotherService.active = true;
-        anotherService.cost = 300.00
+        anotherService.cost = 900.00
         anotherService.period = "2015-01"
         anotherService.stDate = new Date()
         anotherService.endDate = new Date()
-        anotherService.typeService = typeService
+        anotherService.typeService = anothertypeService
         anotherService.save()
+
+        Service abcService = new Service()
+        abcService.active = true;
+        abcService.cost = 600.00
+        abcService.period = "2015-01"
+        abcService.stDate = new Date()
+        abcService.endDate = new Date()
+        abcService.typeService = othertypeService
+        abcService.save()
 
         StudentService studentService = new StudentService()
         studentService.service = service
         studentService.user = pepo
         studentService.active = true
-        studentService.fullPayment = 1500.00
+        studentService.fullPayment = 500.00
         studentService.save()
 
         StudentService anotherstudentService = new StudentService()
         anotherstudentService.service = anotherService
         anotherstudentService.user = pepo
         anotherstudentService.active = true
-        anotherstudentService.fullPayment = 1500.00
-        anotherstudentService.save(flush: true)
+        anotherstudentService.fullPayment = 500.00
+        anotherstudentService.save()
+
+        StudentService SecondStudentService = new StudentService()
+        SecondStudentService.service = abcService
+        SecondStudentService.user = pepo
+        SecondStudentService.active = true
+        SecondStudentService.fullPayment = 500.00
+        SecondStudentService.save()
+
+        StudentService studentService_1 = new StudentService()
+        studentService_1.service = service
+        studentService_1.user = user
+        studentService_1.active = true
+        studentService_1.fullPayment = 500.00
+        studentService_1.save()
+
+        StudentService studentService_2 = new StudentService()
+        studentService_2.service = anotherService
+        studentService_2.user = user
+        studentService_2.active = true
+        studentService_2.fullPayment = 500.00
+        studentService_2.save(flush: true)
+
+        StudentService studentService_3 = new StudentService()
+        studentService_3.service = abcService
+        studentService_3.user = user
+        studentService_3.active = true
+        studentService_3.fullPayment = 500.00
+        studentService_3.save()
 
     }
     def destroy = {
