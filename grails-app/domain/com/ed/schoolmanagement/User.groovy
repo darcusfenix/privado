a99dca35904ed8833885f0b3833bc7775acf9f12
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2015-07-13 14:11:56 -0500

    #1, Updating user structure
    Considering new address information, now zipCode, City, Town, etc. fields enabled

--- grails-app/domain/com/ed/schoolmanagement/User.groovy
+++ grails-app/domain/com/ed/schoolmanagement/User.groovy
@@ -5,16 +5,21 @@ class User {
     String name
     String lastName
     String phoneNumber
-    String address
     String username
     String password
     boolean enabled = true
     String mobileNumber
     String email
     String socialNetworkUrl
-
+    boolean type
+    // Address
+    String state
+    String city
+    String town
+    String zipCode
+    String street
+    String streetNumber
     // Spring security methods
-
     @Override
     int hashCode() {
         username?.hashCode() ?: 0
@@ -37,13 +42,19 @@ class User {
     static constraints = {
         username nullable: false, blank: false, unique: true
         password nullable: false, blank: false
-        address nullable: false, blank: false
         name nullable: false, blank: false, maxSize: 100
         lastName nullable: true, blank: true, maxSize: 100
         phoneNumber nullable: false, maxSize: 20
         mobileNumber nullable: true, maxSize: 20
         email nullable: false, blank: false, unique:true, email: true
         socialNetworkUrl nullable: true, blank: true, url: true
+        // Address
+        zipCode nullable: false; blank: false
+        street nullable: false; blank: false
+        streetNumber nullable: false; blank: false
+        city nullable: false; blank: false
+        town nullable: false; blank: false
+        state nullable: true, blank: true
     }
     static mapping = {
         version false
