c7f061b59967be181e3684933fe3ddcb878f417b
Juan <juancvfenix@gmail.com>
2015-07-26 21:56:59 -0500

    #4, CRUD for extra incomes

--- grails-app/controllers/com/service/ExtraIncomeController.groovy
+++ grails-app/controllers/com/service/ExtraIncomeController.groovy
@@ -5,6 +5,8 @@ import com.ed.service.ExtraService
 import com.ed.service.OnlineCourse
 import grails.converters.JSON
 
+import java.text.SimpleDateFormat
+
 class ExtraIncomeController {
 
     static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
@@ -33,7 +35,7 @@ class ExtraIncomeController {
         if (extraIncomeInstance.validate()) {
             extraIncomeInstance.save(flush: true)
             response.status = 200
-            render([extraIncomeInstance: extraIncomeInstance, message: message(code: "extraIncomeInstance.created")] as JSON)
+            render([extraIncomeInstance: extraIncomeInstance, message: message(code: "extraIncome.created")] as JSON)
 
         } else {
             response.status = 500
@@ -41,9 +43,44 @@ class ExtraIncomeController {
         }
     }
     def update(){
+        SimpleDateFormat sdf = new SimpleDateFormat ("MM/dd/yyyy HH:mm:ss");
+        def extraIncomeInstance = ExtraIncome.findById(request.JSON.id)
 
+
+        if (ExtraService.findById(request.JSON.extraService.id).cost != request.JSON.income){
+            response.status = 500
+            render([extraIncomeInstance: extraIncomeInstance, error: "La cantidad no corresponde a lo requerido"] as JSON)
+            return  false;
+        }
+
+        def jsonMap = [:]
+
+        jsonMap = request.JSON
+
+        Date incomeDate = sdf.parse(request.JSON.incomeDate)
+
+        extraIncomeInstance.incomeDate = incomeDate
+
+
+        jsonMap.remove('incomeDate')
+
+        extraIncomeInstance.properties = request.JSON
+
+        if (extraIncomeInstance.validate()) {
+
+            extraIncomeInstance.save(flush: true)
+            response.status = 200
+            render([extraIncomeInstance: extraIncomeInstance, message: message(code: "extraIncome.updated")] as JSON)
+        }else{
+            response.status = 500
+            render(extraIncomeInstance.errors as JSON)
+        }
     }
     def delete(){
+        ExtraIncome extraIncome = ExtraIncome.findById(params.int("id"))
+        extraIncome.delete(flush: true)
+        response.status = 200
+        render([success: message(code: 'extraIncome.deleted')] as JSON)
 
     }
 }
