9e135b2786ff4373cd34a0d9903c474ebbda3f84
ftapia23 <ftapia.ipn@gmail.com>
2015-08-17 04:45:05 -0500

    Security, Correction register users

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -38,7 +38,7 @@ class BootStrap {
         def springContext = WebApplicationContextUtils.getWebApplicationContext(servletContext)
         springContext.getBean("customObjectMarshallers").register()
         //
-
+/*
         //Role.findOrSaveWhere([authority: 'ROLE_ALUMNO'])
         Role roleEmployee = Role.findOrSaveWhere([authority: 'ROLE_EMPLEADO'])
         //Role.findOrSaveWhere([authority: 'ROLE_ADMIN'])
@@ -100,7 +100,7 @@ class BootStrap {
         print("------------------")
         print(roleEmployee.authority)
         print("------------------")
-        UserRole.create(employee, roleEmployee, true)
+        UserRole.create(employee, roleEmployee, true)*/
 
     }
     def destroy = {
